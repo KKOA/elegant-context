5800c4cc33f7106b83b60ea9568c25327ba0915d
Keith - Toshiba - GitKraken <ka33145@googlemail.com>
2024-10-04 14:42:42 +0100

    Wrap CartContextProvider around App context jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,17 @@
 import Header from '../Header/Header.jsx';
 import Shop from '../Shop/Shop.jsx';
+import CartContextProvider from '../../store/CartContext.jsx';
 
 function App() {
 	return (
 		<>
-			<Header
-				cart={shoppingCart}
-				onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-			/>
-			<Shop onAddItemToCart={handleAddItemToCart} />
+			<CartContextProvider>
+				<Header
+					cart={shoppingCart}
+					onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+				/>
+				<Shop onAddItemToCart={handleAddItemToCart} />
+			</CartContextProvider>
 		</>
 	);
 }
