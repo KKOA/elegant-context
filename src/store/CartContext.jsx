230e0ce60900d4ece5f7735b6b58ace839cce1c7
Keith - Toshiba - GitKraken <ka33145@googlemail.com>
2024-10-06 11:23:42 +0100

    userReduce
    Replace useState with useReducer
    Create reducer
    move logic for updating the state to reducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -10,8 +10,9 @@
  * 9) export functional component
  * 
  */
-import { createContext, useState } from "react"; //(1)
-import { DUMMY_PRODUCTS } from '../data/dummy-products.js';
+
+import { createContext, /* useState,*/ useReducer } from "react"; //(1)
+import shoppingCartReducer from "./CartReducer.js";
 
 //Default Context (2)
 //One benefit setting default is get better autocompletion when you try access the context from component 
@@ -29,16 +30,48 @@ const CartContext = createContext(defaultCartContext); // 3
 
 export { defaultCartContext, CartContext }; // 4
 
+/*
+Reducer is defined outside of ContextProvider component
+This is because this function should not be recreated whenever the component executes
+Reducer does not need direct access to any values to any values defined or updated in the component.
+*/
 
-const CartContextProvider = (props) => { //5
+const CartContextProvider = ({ children }) => { //5
+
+	/*
+	Alternative approach to managing state using useState is use useReducer instead.
+	useReducer accept three arguments. 
+	- First is reducer function. 
+	- Second is initialArg aka initialState of reducer.
+	- Third is optional initializer function that should return the initial state.
+	
+	If initializer function is not specified, the initial state is set to initialArg. Otherwise, the initial state is set to the result of calling init(initialArg).
+
+	This initialState is important because without the state will undefined when reducer is initialise (before any dispatches).
+
+	Pass our reducer as argument to useReducer
+	Reducer must define outside of function component.
+	Reducer is defined outside of ContextProvider component
+	This is because this function should not be recreated whenever the component executes
+	Reducer does not need direct access to any values to any values defined or updated in the component.
+
+	The useReducer returns array contain to two values
+	First value is state. Second is dispatch function
+	dispatch function is used dispatch action to reducer to modify the state
+	*/
+
+	const initialState = {
+		items: [],
+	};
+	const [shoppingCartState, ShoppingCartDispatch] = useReducer(shoppingCartReducer, initialState);
 
 	//6
-	const [shoppingCart, setShoppingCart] = useState({
-		items: [],
-	});
+	// const [shoppingCart, setShoppingCart] = useState({
+	// 	items: [],
+	// });
 
 	function handleAddItemToCart(id) {
-		setShoppingCart((prevShoppingCart) => {
+		/*setShoppingCart((prevShoppingCart) => {
 			const updatedItems = [...prevShoppingCart.items];
 
 			const existingCartItemIndex = updatedItems.findIndex(
@@ -65,10 +98,11 @@ const CartContextProvider = (props) => { //5
 			return {
 				items: updatedItems,
 			};
-		});
+		});*/
 	}
 
 	function handleUpdateCartItemQuantity(productId, amount) {
+		/*
 		setShoppingCart((prevShoppingCart) => {
 			const updatedItems = [...prevShoppingCart.items];
 			const updatedItemIndex = updatedItems.findIndex(
@@ -90,11 +124,11 @@ const CartContextProvider = (props) => { //5
 			return {
 				items: updatedItems,
 			};
-		});
+		});*/
 	}
 
 	const ctxValue = {
-		items: shoppingCart.items,
+		items: shoppingCartState.items,
 		addItemToCart: handleAddItemToCart,
 		updateItemQuantity: handleUpdateCartItemQuantity
 	}
@@ -102,7 +136,7 @@ const CartContextProvider = (props) => { //5
 	//7
 	return (
 		<CartContext.Provider value={ctxValue}>
-			{props.children}
+			{children}
 		</CartContext.Provider>
 	);
 }
---
+++ src/store/CartReducer.js
@@ -0,0 +1,74 @@
+/**
+ * useReducer setup
+ * 1) Create reducer function.
+ * 2) Reducer function takes two arguments state and action and return either current state or update statement
+ * 3) Must not directly state but create copy which you can edit and return
+ * 4) import userReducer from react
+ * 5) Pass reducer and initial state to use useReducer
+ * 6) useReducer return state and dispatch function
+ * 7) use dispatch function to pass action to reducer to change state.
+ * In this case define reducer in one file and use in another file
+ */
+
+import { DUMMY_PRODUCTS } from '../data/dummy-products.js';
+
+//state is latest state
+//action used update state
+const shoppingCartReducer = (state, action) => {
+	let updatedItems;
+	switch (action.type) {
+		case "ADD_ITEM":
+			updatedItems = [...state.items];
+
+			const existingCartItemIndex = updatedItems.findIndex(
+				(cartItem) => cartItem.id === action.payload.id
+			);
+			const existingCartItem = updatedItems[existingCartItemIndex];
+
+			if (existingCartItem) {
+				const updatedItem = {
+					...existingCartItem,
+					quantity: existingCartItem.quantity + 1,
+				};
+				updatedItems[existingCartItemIndex] = updatedItem;
+			} else {
+				const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload.id);
+				updatedItems.push({
+					id: action.payload.id,
+					name: product.title,
+					price: product.price,
+					quantity: 1,
+				});
+			}
+
+			return {
+				...state,
+				items: updatedItems,
+			};
+		case "UPDATE_ITEM_QUANTITY":
+			updatedItems = [...state.items];
+			const updatedItemIndex = updatedItems.findIndex(
+				(item) => item.id === action.payload.productId
+			);
+
+			const updatedItem = {
+				...updatedItems[updatedItemIndex],
+			};
+
+			updatedItem.quantity += action.payload.amount;
+
+			if (updatedItem.quantity <= 0) {
+				updatedItems.splice(updatedItemIndex, 1);
+			} else {
+				updatedItems[updatedItemIndex] = updatedItem;
+			}
+
+			return {
+				...state,
+				items: updatedItems,
+			};
+	}
+	return state;
+}
+
+export default shoppingCartReducer;
